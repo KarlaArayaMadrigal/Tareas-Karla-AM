35ea16e8f8b944a6953d599d295605b0a5ecf329
Plataformas Abiertas <kd.araya.m@gmail.com>
2024-09-25 19:17:37 -0600

    Contenido página

--- FrontEnd/index.php
+++ FrontEnd/index.php
@@ -4,8 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Primer Práctica</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+
+    <img
+    src="./img/paisaje.jpg"
+    class="paisaje"
+    alt="Paisaje"
+    />
+    <img
+        src="./img/ternurines.png"
+        class="ternurines"
+        alt="Ternurines"
+    />
+
 </body>
 </html>
